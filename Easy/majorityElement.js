89915d8cc81c221f891c378586227ae42d4387e6
rcmtcristian <rcmtcristian@gmail.com>
2023-07-16 20:44:42 -0400

    ✨ Combine Two Tables

--- Easy/majorityElement.js
+++ Easy/majorityElement.js
@@ -101,3 +101,10 @@ function majorityElement(nums) {
 
   return majority;
 }
+
+
+// Alternative 
+var majorityElement = function(nums) {
+    nums.sort((a, b) => a - b);
+    return nums[parseInt(nums.length / 2)];
+};
